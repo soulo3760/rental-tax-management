6036f9c26c57f358ae182d89329b7d8c56fbc33d
KabodGarments <kabodgarments@gmail.com>
2019-03-06 14:08:26 +0300

    Created functions, arrays to be used in the BL

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ let inputs = document.querySelectorAll("input");
 
 //return output via UI//
 let outputs = document.querySelectorAll("#output>div>span");
+//create a functio//
+//create an array of 5 tax ranges//
+//anything over 466,704 k we use number.MAX_VALUE//
+//create an array of 5 minimum taxes for each tax range/bracket//
+//create an array of 5 tax rates//
+//create an empty arrayan call it "input values"//
+function cal_Tax() {
+  let Input_Values = [];
+  let Income_Ranges = [0, 121968, 236880, 351792, 466704, Number.MAX_VALUE];
+  let Minimum_Tax = [12196.8, 28425.6, 70358.4, 116767,140011.2];
+  let Tax_Rates = [0.1, 0.15, 0.20, 0.25, 0.30,];
+  let Tax_Return;
+  let Tax_Payable;
