2e68ee96b71a9d0f9d5f838c43acb8a4666a775d
KabodGarments <kabodgarments@gmail.com>
2019-03-06 14:07:01 +0300

    Started work on the Business Logic. Selectin all inputs and store them into variables. Also collecting and returning input and output

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Business Logic//
+//select all inputs and store them into  variables//
+//collect all input from html UI//
+let inputs = document.querySelectorAll("input");
+
+//return output via UI//
+let outputs = document.querySelectorAll("#output>div>span");
